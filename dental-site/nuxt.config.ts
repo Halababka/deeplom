42941d863d66f32a224b6f33c869045bb987217b
einz <iljapopov01@mail.ru>
2025-04-20 18:42:48 +0300

    add ya metrik

--- dental-site/nuxt.config.ts
+++ dental-site/nuxt.config.ts
@@ -84,6 +84,29 @@ export default defineNuxtConfig({
               "https://yandex.ru/maps/org/dental/135910965001/"
             ]
           })
+        },
+        {
+          type: "text/javascript",
+          innerHTML: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101207644, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+            });
+          `
+        }
+      ],
+      noscript: [
+        {
+          innerHTML: `
+            <div><img src="https://mc.yandex.ru/watch/101207644" style="position:absolute; left:-9999px;" alt="" /></div>
+          `
         }
       ],
       meta: [
